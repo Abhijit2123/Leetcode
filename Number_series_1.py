a5886cf0969e834ab13cd972fae2e6572f40f76c
Abhijit2123 <80571093+Abhijit2123@users.noreply.github.com>
2021-04-16 18:29:04 +0530

    Create Number_series_1.py

---
+++ Number_series_1.py
@@ -0,0 +1,20 @@
+class term():
+    def findTerm(N):
+        a = 0
+        b = 0
+        if N == 1:
+            return a
+        if N == 2:
+            return b
+        if N > 2:
+            if (N-2) % 2 != 0:
+                term = 7 + ((N-3)//2)*7
+                return term
+            else:
+                term = 6 + ((N-3)//2)*6
+                return term
+
+T = term
+N = int(input("Please input term: "))
+test = T.findTerm(N)
+print("The {0}th term is {1}".format(N,test))
